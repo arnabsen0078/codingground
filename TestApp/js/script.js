6fde52b7997d7002232ecb081b0131e7892e0206
arnabdelgence <arnab.delgence@gmail.com>
2016-09-07 19:24:07 +0530

    new angular file created

--- TestApp/js/script.js
+++ TestApp/js/script.js
@@ -4,18 +4,32 @@ var testModule = angular.module('testmodule', []);
 
 testModule
    .controller('StatusController1',
-    ['$rootScope', '$scope', 'myservice',
+    ['$rootScope' ,'$scope', 'myservice',
     function ($rootScope, $scope, myservice) {
-       $scope.myservice = myservice;   
+       	$scope.myservice = myservice;   
+		$scope.counter = 0;
+		$scope.count = function (inc) {
+			$scope.counter += inc;
+			$rootScope.marker = $scope.counter
+			console.log("scope >> "+$scope.counter);
+			
+		};
+		
     }]);
-
 testModule
    .controller('StatusController2',
-    ['$rootScope', '$scope', 'myservice',
+    ['$rootScope' ,'$scope', 'myservice',
     function ($rootScope, $scope, myservice) {
       $scope.myservice = myservice;
     }]);
-    
+testModule
+   .controller('StatusController3',
+    ['$rootScope' ,'$scope',
+    function ($rootScope, $scope, myservice) {
+		var a = $rootScope.marker;
+		console.log("rootscope >> "+a);
+      	$scope.value = a;
+    }]);
 testModule
     .service('myservice', function() {
       this.xxx = "yyy";
